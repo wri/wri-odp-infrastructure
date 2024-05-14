e46b3708b60a5c222cf89077615482064b678cec
MuhammadIsmailShahzad <ismailshahzadmirza@gmail.com>
2024-05-15 02:00:48 +0500

    Remove unused ECR

--- k8s-infrastructure/modules/ecr/main.tf
+++ k8s-infrastructure/modules/ecr/main.tf
@@ -9,11 +9,11 @@ resource "aws_ecr_repository" "odp-ecr" {
 
 # This ECR is not being used and we are going to remove in future
 # once we verify no service is depending on it.
-resource "aws_ecr_repository" "default" {
+/* resource "aws_ecr_repository" "default" {
   name                 = "${var.cluster_name}-ecr"
   image_tag_mutability = var.ecr.mutability
   image_scanning_configuration {
     scan_on_push = var.ecr.scan_on_push
   }
-}
+} */
 
