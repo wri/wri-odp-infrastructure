86f897fcda6c0cdf3dedfbcfbf9f41c63d85072f
Carlos Del Real <carloshdelreal@gmail.com>
2025-11-13 15:24:22 +0100

    feat: added admin_role from AWSAdministratorAccess

--- k8s-infrastructure/modules/eks/access_entry.tf
+++ k8s-infrastructure/modules/eks/access_entry.tf
@@ -9,4 +9,17 @@ resource "aws_eks_access_entry" "admins" {
   depends_on = [module.eks]
 }
 
+data aws_iam_roles admin_arn {
+  name_regex = "^AWSReservedSSO_AWSAdministratorAccess_(?P<slug>[0-9a-f]{16})$$"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
 
+resource "aws_eks_access_entry" "admin_role" {
+  for_each = data.aws_iam_roles.admin_arn.arns
+
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = each.value
+  type          = "STANDARD"
+
+  depends_on = [module.eks]
+}
