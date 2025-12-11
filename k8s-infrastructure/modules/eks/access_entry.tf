bb22c360a3f436ce72b5ed19a1796220eb2f5a77
Carlos Del Real <carloshdelreal@gmail.com>
2025-12-11 09:49:29 -0500

    feat: get rid of clashing versioning in the resources

--- k8s-infrastructure/modules/eks/access_entry.tf
+++ k8s-infrastructure/modules/eks/access_entry.tf
@@ -2,9 +2,7 @@ resource "aws_eks_access_entry" "admins" {
   cluster_name  = var.cluster_name
   principal_arn = module.eks_admins_iam_role.iam_role_arn
   user_name     = module.eks_admins_iam_role.iam_role_name
-  kubernetes_groups = [
-    "system:masters",
-  ]
+  type          = "STANDARD"
 
   depends_on = [module.eks]
 }
